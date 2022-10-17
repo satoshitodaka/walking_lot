f7dd892635dc82ca4b80ce258692b1328b910374
satoshitodaka <satoshitodaka0705@gmail.com>
2022-10-17 19:08:44 +0900

    add ログアウトのスペック・ログイン処理の切り出し

--- spec/system/login_spec.rb
+++ spec/system/login_spec.rb
@@ -1,14 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe "ログイン", type: :system do
+  let(:user) { create(:user) }
   describe 'ログイン機能' do
     let(:user) { create(:user) }
     context '入力内容に誤りがある場合' do
       it 'エラーメッセージが表示されること' do
         visit '/login'
-        fill_in 'メールアドレス', with: 'wrong@sample.com'
-        fill_in 'パスワード', with: 'hogehoge'
-        click_on 'ログインする'
+        within '#login-form' do
+          fill_in 'メールアドレス', with: 'wrong@sample.com'
+          fill_in 'パスワード', with: 'hogehoge'
+          click_on 'ログイン'
+        end
         expect(page).to have_content 'ログインに失敗しました'
       end
     end
@@ -16,11 +19,24 @@ RSpec.describe "ログイン", type: :system do
     context '入力内容が正しい場合' do
       it 'ログインができること' do
         visit '/login'
-        fill_in 'メールアドレス', with: user.email
-        fill_in 'パスワード', with: 'password' 
-        click_on 'ログインする'
+        within '#login-form' do
+          fill_in 'メールアドレス', with: user.email
+          fill_in 'パスワード', with: 'password' 
+          click_on 'ログイン'
+        end
         expect(page).to have_content 'ログインしました'
       end
+    end
+  end
+
+  describe 'ログアウト機能' do
+    before do
+      login_as(user)
+    end
+    it 'ログアウトできること' do
+      find('#header-avatar-dropdown').click
+      accept_confirm { click_on 'ログアウト' }
+      expect(page).to have_content 'ログアウトしました'
     end
   end
 end
