3743716f5b6a4d784be5c157beb37b6a21cf4cfb
satoshitodaka <satoshitodaka0705@gmail.com>
2022-10-17 19:40:24 +0900

    add annotateを適用した

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,3 +1,20 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id               :bigint           not null, primary key
+#  admin            :boolean          default(FALSE), not null
+#  crypted_password :string(255)
+#  email            :string(255)      not null
+#  name             :string(255)      not null
+#  salt             :string(255)
+#  created_at       :datetime         not null
+#  updated_at       :datetime         not null
+#
+# Indexes
+#
+#  index_users_on_email  (email) UNIQUE
+#
 FactoryBot.define do
   factory :user do
     email { Faker::Internet.unique.email }
