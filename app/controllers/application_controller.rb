558e9cd5d0d1aadd186319eda7d759a562b3c874
satoshitodaka <satoshitodaka0705@gmail.com>
2022-10-24 22:39:15 +0900

    add rails_adminの設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::Base
   add_flash_types :primary, :success, :info, :warning, :danger
+
+  def not_authenticated
+    redirect_to main_app.login_path
+  end
 end
