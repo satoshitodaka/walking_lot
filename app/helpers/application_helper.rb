c1ebb228a76e57bbb3688468109eb39bcb445d65
satoshitodaka <satoshitodaka0705@gmail.com>
2022-11-02 21:43:51 +0900

    add メタタグの実装・OGPとファビコン画像を追加

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,24 @@
 module ApplicationHelper
+  def default_meta_tags
+    {
+      site: Settings.meta.site,
+      title: Settings.meta.title,
+      reverse: true,
+      description: Settings.meta.description,
+      keywords: Settings.meta.keywords,
+      canonical: request.original_url,
+      og: {
+        title: :full_title,
+        type: Settings.meta.og.type,
+        url: request.original_url,
+        image: image_url(Settings.meta.og.image_path),
+        site_name: :site,
+        description: :description,
+        locale: 'ja_JP'
+      },
+      twitter: {
+        card: 'summary_large_image'
+      }
+    }
+  end
 end
