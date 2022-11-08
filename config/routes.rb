1d4c7ee7ffc3f093dff65a7c579473aad74fe466
satoshitodaka <satoshitodaka0705@gmail.com>
2022-11-08 18:53:12 +0900

    add 通知の既読にできるようにした

--- config/routes.rb
+++ config/routes.rb
@@ -25,6 +25,7 @@
 #                                          PATCH  /mypage/account(.:format)                                                                         mypage/accounts#update
 #                                          PUT    /mypage/account(.:format)                                                                         mypage/accounts#update
 #                                          DELETE /mypage/account(.:format)                                                                         mypage/accounts#destroy
+#                        notification_read POST   /notifications/:notification_id/read(.:format)                                                    notifications/reads#create
 #                                    about GET    /about(.:format)                                                                                  static_pages#about
 #                                  privacy GET    /privacy(.:format)                                                                                static_pages#privacy
 #                                    rules GET    /rules(.:format)                                                                                  static_pages#rules
@@ -87,6 +88,10 @@ Rails.application.routes.draw do
     resources :notifications, only: %i[index]
     resources :activities
     resource :account, only: %i[show edit update destroy]
+  end
+
+  resources :notifications, only: [] do
+    resource :read, only: %i[create], module: :notifications
   end
 
   get '/about', to: 'static_pages#about'
