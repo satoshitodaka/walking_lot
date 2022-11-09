91f92a9af7fd86b9aa225f89a50bcb610b9f7109
satoshitodaka <satoshitodaka0705@gmail.com>
2022-11-10 08:00:33 +0900

    add 管理者宛の承認依頼メールを実装

--- app/mailers/admin_mailer.rb
+++ app/mailers/admin_mailer.rb
@@ -1,2 +1,10 @@
 class AdminMailer < ApplicationMailer
+  default to: -> { User.admin.pluck(:email) },
+          from: 'admin_notification@walking_lot.com'
+  
+  def new_activity
+    @user = params[:user]
+    @activity = params[:activity]
+    mail(subject: '作成されたアクティビティを承認してください')
+  end
 end
