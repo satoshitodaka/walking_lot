e5b35d38e1c94968ec45fc5541ebb61424c2a5d8
satoshitodaka <satoshitodaka0705@gmail.com>
2022-11-20 00:56:31 +0900

    add Sidekiqにベーシック認証を追加

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -5,3 +5,8 @@ end
 Sidekiq.configure_client do |config|
   config.redis = { url: ENV['REDIS_URL'] }
 end
+
+require 'sidekiq/web'
+Sidekiq::Web.use(Rack::Auth::Basic) do |user, password|
+  [user, password] == [ENV['SIDEKIQ_ADMIN_BASIC_AUTH_USER'], ENV['SIDEKIQ_ADMIN_BASIC_AUTH_PASSWORD']]
+end
