bab60b3110f917054a8b85eb58689e3e5052ef51
satoshitodaka <satoshitodaka0705@gmail.com>
2022-11-23 12:17:44 +0900

    fix オリジナルのエラーページを削除
    ActiveStorageのファイルにアクセスする際に404エラーが発生するため。
    回避策は存在するが、意図しない挙動を避けるために今回は削除する。

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,16 +8,4 @@ class ApplicationController < ActionController::Base
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to main_app.root_path, warning: exception.message
   end
-
-  # rescue_from ActiveRecord::RecordNotFound, with: :render_404
-  # rescue_from ActionController::RoutingError, with: :render_404
-  # rescue_from Exception, with: :render_500
-
-  # def render_404
-  #   render template: 'errors/error_404', status: 404, layout: 'application', content_type: 'text/html'
-  # end
-
-  # def render_500
-  #   render template: 'errors/error_500', status: 500, layout: 'application', content_type: 'text/html'
-  # end
 end
