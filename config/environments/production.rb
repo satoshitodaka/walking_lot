1103fc74196eca66b67fd0070b357265123f6f3b
satoshitodaka <satoshitodaka0705@gmail.com>
2022-11-26 19:33:10 +0900

    fix Sendgridのドメインの設定を修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,7 +94,7 @@ Rails.application.configure do
   ActionMailer::Base.smtp_settings = {
     user_name: 'apikey', # This is the string literal 'apikey', NOT the ID of your API key
     password: '<SENDGRID_API_KEY>', # This is the secret sendgrid API key which was issued during API key creation
-    domain: 'walking-lot.com',
+    domain: 'heroku.com',
     address: 'smtp.sendgrid.net',
     port: 587,
     authentication: :plain,
