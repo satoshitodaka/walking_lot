05952fbe5b0a8bacbaa7b032c24b9f4eaf1603df
satoshitodaka <satoshitodaka0705@gmail.com>
2022-11-27 00:05:15 +0900

    fix メールの設定誤り修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -99,7 +99,7 @@ Rails.application.configure do
   ActionMailer::Base.smtp_settings = {
     user_name: 'apikey', # This is the string literal 'apikey', NOT the ID of your API key
     password: ENV['<SENDGRID_API_KEY>'], # This is the secret sendgrid API key which was issued during API key creation
-    domain: 'walking-lot.com',
+    domain: 'www.walking-lot.com',
     address: 'smtp.sendgrid.net',
     port: 587,
     authentication: :plain,
