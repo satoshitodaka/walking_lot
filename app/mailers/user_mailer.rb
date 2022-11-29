7d992541b1268c7d53f7cf349d43ed5f81973170
satoshitodaka <satoshitodaka0705@gmail.com>
2022-11-29 12:26:31 +0900

    add パスワードリセット機能を実装

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,4 +3,10 @@ class UserMailer < ApplicationMailer
     @user = params[:user] 
     mail(to: @user.email, subject: '散歩くじへようこそ！')
   end
+
+  def reset_password_email(user)
+    @user = User.find(user.id)
+    @url = edit_password_reset_url(@user.reset_password_token)
+    mail( to: @user.email, subject: 'パスワードリセット | 散歩くじ')
+  end
 end
