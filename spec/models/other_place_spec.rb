ad8df442862fc068abdb0346c5746074315f0daf
satoshitodaka <satoshitodaka0705@gmail.com>
2022-12-01 13:56:37 +0900

    add OtherPlaceのモデルスペックを書いた

--- spec/models/other_place_spec.rb
+++ spec/models/other_place_spec.rb
@@ -20,5 +20,36 @@
 require 'rails_helper'
 
 RSpec.describe OtherPlace, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # let!(:lot) = { create(:lot) }
+  describe 'バリデーション' do
+    it 'lot_idが必須であること' do
+      other_place = build(:other_place, lot_id: nil)
+      other_place.valid?
+      expect(other_place.errors[:lot]).to include('を入力してください')
+    end
+
+    it 'nameが必須であること' do
+      other_place = build(:other_place, name: nil)
+      other_place.valid?
+      expect(other_place.errors[:name]).to include('を入力してください')
+    end
+
+    it 'addressが必須であること' do
+      other_place = build(:other_place, address: nil)
+      other_place.valid?
+      expect(other_place.errors[:address]).to include('を入力してください')
+    end
+
+    it 'place_numberが必須であること' do
+      other_place = build(:other_place, place_number: nil)
+      other_place.valid?
+      expect(other_place.errors[:place_number]).to include('を入力してください')
+    end
+
+    xit 'lot_idとplace_numberの組み合わせが一意であること' do
+      other_place = build(:other_place, lot_id: 'sample1', place_number: 1)
+      other_place.valid?
+      expect(other_place.errors[:place_number]).to include('はすでに存在します')
+    end
+  end
 end
