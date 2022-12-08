d83bc3db9987976a44de4f798e5403a61a91b642
satoshitodaka <satoshitodaka0705@gmail.com>
2022-12-08 14:27:49 +0900

    fix スペックファイルの修正

--- spec/mailers/activity_spec.rb
+++ spec/mailers/activity_spec.rb
@@ -7,11 +7,12 @@ RSpec.describe ActivityMailer, type: :mailer do
     let!(:mail) { ActivityMailer.with(activity: activity).update_activity }
 
     it 'メールが正しく作成されること' do
+      # debugger
       expect(mail.to).to eq [user.email]
       expect(mail.from).to eq ['support@walking-lot.com']
       expect(mail.subject).to eq 'あなたのアクティビティが更新されました | 散歩くじ'
-      expect(mail.body).to match [user.name]
-      expect(mail.body).to match ['あなたのアクティビティが更新（承認・公開）されました。']
+      expect(mail.body).to match user.name
+      expect(mail.body).to match 'あなたのアクティビティが更新（承認・公開）されました。'
     end
   end
 end
--- spec/mailers/admin_spec.rb
+++ spec/mailers/admin_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe AdminMailer, type: :mailer do
     let!(:activity) { create(:activity, user: user) }
     let!(:mail) { AdminMailer.with(user: user, activity: activity).approval_required }
     it '管理者に承認依頼通知メールが送信される' do
-      expect(mail.subject).to eq '作成されたアクティビティを承認してください'
+      expect(mail.subject).to eq 'アクティビティを承認してください | 散歩くじ'
       expect(mail.to).to eq [admin_user.email]
       expect(mail.from).to eq ['support@walking-lot.com']
       expect(mail.body).to match("#{user.name}さんがアクティビティを新規作成しました。ログインして承認・公開してください。")
