65740e9655710a3bbbfbafaf1d01da9065515ec7
satoshitodaka <satoshitodaka0705@gmail.com>
2022-12-08 10:39:21 +0900

    add Activity更新時の通知を作成

--- app/controllers/admin/activities_controller.rb
+++ app/controllers/admin/activities_controller.rb
@@ -15,6 +15,7 @@ class Admin::ActivitiesController < Admin::BaseController
     @activity = Activity.find(params[:id])
 
     if @activity.update(activitiy_params)
+      create_notification_about_activity_updated(@activity)
       ActivityMailer.with(activity: @activity).update_activity.deliver_later
       redirect_to admin_activity_path, success: 'アクティビティを更新しました'
     else
@@ -32,5 +33,13 @@ class Admin::ActivitiesController < Admin::BaseController
 
   def activitiy_params
     params.require(:activity).permit(:content, { location_type_ids: [] }, :approved, :released)
+  end
+
+  def create_notification_about_activity_updated(activity)
+    notification = Notification.create!(
+      title: 'アクティビティが更新されました',
+      url: mypage_activity_path(activity)
+    )
+    notification.notify(activity.user)
   end
 end
