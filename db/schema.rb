fbb1ace5f89f6b29027fdb856f3343893b23b92b
satoshitodaka <satoshitodaka0705@gmail.com>
2022-12-18 14:36:08 +0900

    remove OtherPlaceからplace_numberを削除

--- db/schema.rb
+++ db/schema.rb
@@ -10,153 +10,151 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_04_043745) do
-  create_table 'active_storage_attachments', charset: 'utf8mb4', collation: 'utf8mb4_0900_ai_ci', force: :cascade do |t|
-    t.string 'name', null: false
-    t.string 'record_type', null: false
-    t.bigint 'record_id', null: false
-    t.bigint 'blob_id', null: false
-    t.datetime 'created_at', null: false
-    t.index ['blob_id'], name: 'index_active_storage_attachments_on_blob_id'
-    t.index ['record_type', 'record_id', 'name', 'blob_id'], name: 'index_active_storage_attachments_uniqueness', unique: true
+ActiveRecord::Schema[7.0].define(version: 2022_12_18_052129) do
+  create_table "active_storage_attachments", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "record_type", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
+    t.datetime "created_at", null: false
+    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
   end
 
-  create_table 'active_storage_blobs', charset: 'utf8mb4', collation: 'utf8mb4_0900_ai_ci', force: :cascade do |t|
-    t.string 'key', null: false
-    t.string 'filename', null: false
-    t.string 'content_type'
-    t.text 'metadata'
-    t.string 'service_name', null: false
-    t.bigint 'byte_size', null: false
-    t.string 'checksum'
-    t.datetime 'created_at', null: false
-    t.index ['key'], name: 'index_active_storage_blobs_on_key', unique: true
+  create_table "active_storage_blobs", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "key", null: false
+    t.string "filename", null: false
+    t.string "content_type"
+    t.text "metadata"
+    t.string "service_name", null: false
+    t.bigint "byte_size", null: false
+    t.string "checksum"
+    t.datetime "created_at", null: false
+    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
-  create_table 'active_storage_variant_records', charset: 'utf8mb4', collation: 'utf8mb4_0900_ai_ci', force: :cascade do |t|
-    t.bigint 'blob_id', null: false
-    t.string 'variation_digest', null: false
-    t.index ['blob_id', 'variation_digest'], name: 'index_active_storage_variant_records_uniqueness', unique: true
+  create_table "active_storage_variant_records", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.bigint "blob_id", null: false
+    t.string "variation_digest", null: false
+    t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
-  create_table 'activities', charset: 'utf8mb4', collation: 'utf8mb4_0900_ai_ci', force: :cascade do |t|
-    t.text 'content', null: false
-    t.boolean 'released', default: false, null: false
-    t.boolean 'approved', default: false, null: false
-    t.bigint 'user_id'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['user_id'], name: 'index_activities_on_user_id'
+  create_table "activities", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.text "content", null: false
+    t.boolean "released", default: false, null: false
+    t.boolean "approved", default: false, null: false
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_activities_on_user_id"
   end
 
-  create_table 'activity_location_types', charset: 'utf8mb4', collation: 'utf8mb4_0900_ai_ci', force: :cascade do |t|
-    t.bigint 'activity_id', null: false
-    t.bigint 'location_type_id', null: false
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['activity_id', 'location_type_id'], name: 'unique_index_on_activity_location_types', unique: true
-    t.index ['activity_id'], name: 'index_activity_location_types_on_activity_id'
-    t.index ['location_type_id'], name: 'index_activity_location_types_on_location_type_id'
+  create_table "activity_location_types", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.bigint "activity_id", null: false
+    t.bigint "location_type_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["activity_id", "location_type_id"], name: "unique_index_on_activity_location_types", unique: true
+    t.index ["activity_id"], name: "index_activity_location_types_on_activity_id"
+    t.index ["location_type_id"], name: "index_activity_location_types_on_location_type_id"
   end
 
-  create_table 'authentications', charset: 'utf8mb4', collation: 'utf8mb4_0900_ai_ci', force: :cascade do |t|
-    t.integer 'user_id', null: false
-    t.string 'provider', null: false
-    t.string 'uid', null: false
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['provider', 'uid'], name: 'index_authentications_on_provider_and_uid'
+  create_table "authentications", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.string "provider", null: false
+    t.string "uid", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["provider", "uid"], name: "index_authentications_on_provider_and_uid"
   end
 
-  create_table 'location_types', charset: 'utf8mb4', collation: 'utf8mb4_0900_ai_ci', force: :cascade do |t|
-    t.integer 'location_type', null: false
-    t.string 'name', null: false
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['location_type'], name: 'index_location_types_on_location_type', unique: true
+  create_table "location_types", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.integer "location_type", null: false
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["location_type"], name: "index_location_types_on_location_type", unique: true
   end
 
-  create_table 'lot_activities', charset: 'utf8mb4', collation: 'utf8mb4_0900_ai_ci', force: :cascade do |t|
-    t.bigint 'activity_id', null: false
-    t.string 'lot_id', null: false
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['activity_id'], name: 'index_lot_activities_on_activity_id'
-    t.index ['lot_id', 'activity_id'], name: 'index_lot_activities_on_lot_id_and_activity_id', unique: true
+  create_table "lot_activities", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.bigint "activity_id", null: false
+    t.string "lot_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["activity_id"], name: "index_lot_activities_on_activity_id"
+    t.index ["lot_id", "activity_id"], name: "index_lot_activities_on_lot_id_and_activity_id", unique: true
   end
 
-  create_table 'lots', id: { type: :string, limit: 36 }, charset: 'utf8mb4', collation: 'utf8mb4_0900_ai_ci', force: :cascade do |t|
-    t.string 'start_point_name'
-    t.string 'start_point_address'
-    t.float 'start_point_latitude', null: false
-    t.float 'start_point_longitude', null: false
-    t.string 'destination_name'
-    t.string 'destination_address'
-    t.float 'destination_latitude'
-    t.float 'destination_longitude'
-    t.json 'nearby_locations'
-    t.bigint 'user_id'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.bigint 'location_type_id', null: false
-    t.string 'photo_url'
-    t.index ['user_id'], name: 'index_lots_on_user_id'
+  create_table "lots", id: { type: :string, limit: 36 }, charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "start_point_name"
+    t.string "start_point_address"
+    t.float "start_point_latitude", null: false
+    t.float "start_point_longitude", null: false
+    t.string "destination_name"
+    t.string "destination_address"
+    t.float "destination_latitude"
+    t.float "destination_longitude"
+    t.json "nearby_locations"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "location_type_id", null: false
+    t.string "photo_url"
+    t.index ["user_id"], name: "index_lots_on_user_id"
   end
 
-  create_table 'notifications', charset: 'utf8mb4', collation: 'utf8mb4_0900_ai_ci', force: :cascade do |t|
-    t.string 'title', null: false
-    t.string 'url', null: false
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "notifications", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "title", null: false
+    t.string "url", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table 'other_places', charset: 'utf8mb4', collation: 'utf8mb4_0900_ai_ci', force: :cascade do |t|
-    t.integer 'place_number', null: false
-    t.string 'lot_id', null: false
-    t.string 'name', null: false
-    t.string 'address', null: false
-    t.float 'latitude', null: false
-    t.float 'longitude', null: false
-    t.string 'photo_url'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['lot_id', 'place_number'], name: 'index_other_places_on_lot_id_and_place_number', unique: true
+  create_table "other_places", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "lot_id", null: false
+    t.string "name", null: false
+    t.string "address", null: false
+    t.float "latitude", null: false
+    t.float "longitude", null: false
+    t.string "photo_url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table 'user_notifications', charset: 'utf8mb4', collation: 'utf8mb4_0900_ai_ci', force: :cascade do |t|
-    t.bigint 'user_id', null: false
-    t.bigint 'notification_id', null: false
-    t.boolean 'read', default: false, null: false
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['notification_id', 'user_id'], name: 'index_user_notifications_on_notification_id_and_user_id', unique: true
-    t.index ['notification_id'], name: 'index_user_notifications_on_notification_id'
-    t.index ['user_id'], name: 'index_user_notifications_on_user_id'
+  create_table "user_notifications", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "notification_id", null: false
+    t.boolean "read", default: false, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["notification_id", "user_id"], name: "index_user_notifications_on_notification_id_and_user_id", unique: true
+    t.index ["notification_id"], name: "index_user_notifications_on_notification_id"
+    t.index ["user_id"], name: "index_user_notifications_on_user_id"
   end
 
-  create_table 'users', charset: 'utf8mb4', collation: 'utf8mb4_0900_ai_ci', force: :cascade do |t|
-    t.string 'name', null: false
-    t.string 'email'
-    t.string 'crypted_password'
-    t.string 'salt'
-    t.boolean 'admin', default: false, null: false
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.string 'reset_password_token'
-    t.datetime 'reset_password_token_expires_at'
-    t.datetime 'reset_password_email_sent_at'
-    t.integer 'access_count_to_reset_password_page', default: 0
-    t.index ['email'], name: 'index_users_on_email', unique: true
-    t.index ['reset_password_token'], name: 'index_users_on_reset_password_token'
+  create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "email"
+    t.string "crypted_password"
+    t.string "salt"
+    t.boolean "admin", default: false, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_token_expires_at"
+    t.datetime "reset_password_email_sent_at"
+    t.integer "access_count_to_reset_password_page", default: 0
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token"
   end
 
-  add_foreign_key 'active_storage_attachments', 'active_storage_blobs', column: 'blob_id'
-  add_foreign_key 'active_storage_variant_records', 'active_storage_blobs', column: 'blob_id'
-  add_foreign_key 'activities', 'users'
-  add_foreign_key 'activity_location_types', 'activities'
-  add_foreign_key 'activity_location_types', 'location_types'
-  add_foreign_key 'lot_activities', 'activities'
-  add_foreign_key 'lots', 'users'
-  add_foreign_key 'user_notifications', 'notifications'
-  add_foreign_key 'user_notifications', 'users'
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "activities", "users"
+  add_foreign_key "activity_location_types", "activities"
+  add_foreign_key "activity_location_types", "location_types"
+  add_foreign_key "lot_activities", "activities"
+  add_foreign_key "lots", "users"
+  add_foreign_key "user_notifications", "notifications"
+  add_foreign_key "user_notifications", "users"
 end
--- spec/models/other_place_spec.rb
+++ spec/models/other_place_spec.rb
@@ -2,20 +2,15 @@
 #
 # Table name: other_places
 #
-#  id           :bigint           not null, primary key
-#  address      :string(255)      not null
-#  latitude     :float(24)        not null
-#  longitude    :float(24)        not null
-#  name         :string(255)      not null
-#  photo_url    :string(255)
-#  place_number :integer          not null
-#  created_at   :datetime         not null
-#  updated_at   :datetime         not null
-#  lot_id       :string(255)      not null
-#
-# Indexes
-#
-#  index_other_places_on_lot_id_and_place_number  (lot_id,place_number) UNIQUE
+#  id         :bigint           not null, primary key
+#  address    :string(255)      not null
+#  latitude   :float(24)        not null
+#  longitude  :float(24)        not null
+#  name       :string(255)      not null
+#  photo_url  :string(255)
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  lot_id     :string(255)      not null
 #
 require 'rails_helper'
 
